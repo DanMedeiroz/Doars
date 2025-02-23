de77e1e8c2f22f801a609636ba2e91202fe0cb28
Pichee <gustavopichcavalho@gmail.com>
2025-02-23 11:22:59 -0300

    Add:fazendo o obj beneficiario

---
+++ Back/src/main/java/Beneficiario/Dto_Beneficiario.java
@@ -0,0 +1,9 @@
+package Beneficiario;
+public class Dto_Beneficiario {
+    private long id;
+    private String nome;
+    private String cnpj;
+    private String adress;
+    private int telefone;
+    private String site;
+}
