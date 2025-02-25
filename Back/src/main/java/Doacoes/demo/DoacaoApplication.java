4b58f8ba9301ecf41511add98f90c66d47c5260b
Pichee <gustavopichcavalho@gmail.com>
2025-02-24 21:38:42 -0300

    Add:adicionandoi a configuração do sql(FAVOR CASO FOR MEXER NO PROJETO USE OUTRA BRANCH PARA NÃO DAR RUIM

--- Back/src/main/java/Doacoes/demo/DoacaoApplication.java
+++ Back/src/main/java/Doacoes/demo/DoacaoApplication.java
@@ -1,13 +1,11 @@
-package Doacoes.demo;
+package main.java.Doacoes.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "doacoes.demo")
 public class DoacaoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DoacaoApplication.class, args);
 	}
-
 }
