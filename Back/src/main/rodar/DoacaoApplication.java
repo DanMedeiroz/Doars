0e3e5708533b8c2f0b9f7168f909b4aaa7391c0a
Pichee <gustavopichcavalho@gmail.com>
2025-02-26 21:39:27 -0300

    Dando 15 passos para trás para resosvler o problema de não rodar a aplicação,essa semana se der certo eu configuro corretamente o banco de dados e o http

--- Back/src/main/rodar/DoacaoApplication.java
+++ Back/src/main/rodar/DoacaoApplication.java
@@ -1,9 +1,9 @@
-package main.java.Doacoes.demo;
+package main.rodar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "doacoes.demo")
+@SpringBootApplication
 public class DoacaoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DoacaoApplication.class, args);
