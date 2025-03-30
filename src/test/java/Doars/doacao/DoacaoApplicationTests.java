f7071579a5d3f234179f2b13a2c9cf5f859bad84
Pichee <gustavopichcavalho@gmail.com>
2025-03-30 20:00:51 -0300

    esqueleto

---
+++ src/test/java/Doars/doacao/DoacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package Doars.doacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
